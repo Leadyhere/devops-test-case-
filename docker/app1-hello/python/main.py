6aa6cefe940538b5d6a57a46cbcfd080f4ad54d8
Leadyhere <aviralnirgun16@gmail.com>
2026-08-14 21:04:26 +0530

    Add Docker and Kubernetes setup

--- docker/app1-hello/python/main.py
+++ docker/app1-hello/python/main.py
@@ -1,11 +1,8 @@
 from flask import Flask, jsonify
 import os, socket
-
 app = Flask(__name__)
-
 ENV = os.getenv("ENV_VALUE", "No env set")
 HOSTNAME = socket.gethostname()
-
 @app.get("/")
 def hello():
     return jsonify({
@@ -13,6 +10,5 @@ def hello():
         "env": ENV,
         "container": HOSTNAME
     })
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)
